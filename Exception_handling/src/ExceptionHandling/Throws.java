7dbea745bd8303a81486a652c0e521abb9a61645
Tashfi <43473062+tashfi04@users.noreply.github.com>
2019-03-30 09:01:30 +0600

    Added throws example

---
+++ Exception_handling/src/ExceptionHandling/Throws.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExceptionHandling;
+
+/**
+ *
+ * @author Tasfee
+ */
+public class Throws {
+    
+//    static int divide(int a, int b) throws ArithmeticException {
+//        int c;
+//        c = a/b;
+//        
+//        return c;
+//    }
+    
+    public static void main(String[] args) throws ArithmeticException, InterruptedException {
+//        int a = 100;
+//        int b = 0;
+//        int c;
+        
+//        c = a/b;
+        
+//        try {
+//            divide(a, b);
+//        } catch(ArithmeticException ex) {
+//            System.out.println("Arithmetic exception!");
+//        }
+
+          Thread.sleep(100);
+          System.out.println("****");
+    }
+}
