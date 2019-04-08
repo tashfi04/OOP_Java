3012a2625c2d421f41cf7ab144117fef05408753
Tashfi <43473062+tashfi04@users.noreply.github.com>
2019-04-08 08:47:40 +0600

    added file input output

---
+++ Assignment_5/src/com/filereadwrite/wordcount/MainFile.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.filereadwrite.wordcount;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Tasfee
+ */
+public class MainFile {
+    public static void main(String[] args) throws IOException {
+        File f1 = new File("input.txt");
+        File f2 = new File("output.txt");
+    }
+}
